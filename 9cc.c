73f197777b1170a9585ffb1ee63e284a35a316f4
yttnn <67893139+yttnn@users.noreply.github.com>
2022-11-06 09:59:08 +0000

    step6

--- 9cc.c
+++ 9cc.c
@@ -147,6 +147,19 @@ bool at_eof() {
 Node *expr();
 Node *mul();
 Node *primary();
+Node *unary();
+
+// unary   = ("+" | "-")? primary
+// - -10 = 10と解釈するため、+-のケースではunaryを返す
+Node *unary() {
+  if (consume('+')) {
+    return unary();
+  }
+  if (consume('-')) {
+    return new_node(ND_SUB, new_node_num(0), unary());
+  }
+  return primary();
+}
 
 // primary = "(" expr ")" | num
 Node *primary() {
@@ -158,14 +171,14 @@ Node *primary() {
   return new_node_num(expect_number());
 }
 
-// mul = primary ( "*" primary | "/" primary)*
+// mul = unary ( "*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
   for(;;) {
     if (consume('*')) {
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     } else if (consume('/')) {
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     } else {
       return node;
     }
