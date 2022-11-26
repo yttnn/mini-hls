2cd816dbd9e760f34b50f5ac63f373900e8e240f
yttnn <67893139+yttnn@users.noreply.github.com>
2022-11-26 14:15:23 +0000

    fix

--- parse.c
+++ parse.c
@@ -81,7 +81,7 @@ Token *tokenize(char *p) {
           p += 2;
           continue;
       }
-    if (strchr("+-*/()", *p)) {
+    if (strchr("+-*/()<>", *p)) {
       cur = new_token(TK_RESERVED, cur, p++, 1);
       continue;
     }
